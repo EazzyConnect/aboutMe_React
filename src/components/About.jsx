16cca956c035508a4b12e057e946bacef54bb47c
Isaiah Okon <izaya4real@gmail.com>
2024-05-15 12:26:24 +0100

    update on project

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,8 @@
-import React from 'react';
-import AboutMeProps from './sub-components/AboutMeProps';
-import aboutCSS from './css-files/About.module.css';
-
+import React from "react";
+import AboutMeProps from "./sub-components/AboutMeProps";
+import aboutCSS from "./css-files/About.module.css";
 
 const About = () => {
-
   const myExperience = [
     // {
     //   title: "Software Engineer (Backend)",
@@ -17,36 +15,36 @@ const About = () => {
       title: "Software Engineer (Full-Stack)",
       organization: "SAIL Innovation Lab",
       location: "Lagos, Nigeria",
-      date: "Nov 2023 - Current",
-      partFullTrainee: "Part-Time"
+      date: "Nov 2023 - May 2024",
+      partFullTrainee: "Part-Time",
     },
     {
       title: "Software Engineer (Backend)",
       organization: "TechQuest STEM Academy",
       location: "Lagos, Nigeria",
       date: "Feb 2023 - Aug 2023",
-      partFullTrainee: "Trainee"
+      partFullTrainee: "Trainee",
     },
     {
       title: "Software Developer (Frontend)",
       organization: "Zuri Cohort Bootcamp",
       location: "Lagos, Nigeria",
       date: "Oct 2022 - Nov 2022",
-      partFullTrainee: "Trainee"
+      partFullTrainee: "Trainee",
     },
     {
       title: "Academic Tutor",
       organization: "Greater Scholar International School",
       location: "Lagos, Nigeria",
       date: "Sept 2019 - Aug 2022",
-      partFullTrainee: "Full-Time"
+      partFullTrainee: "Full-Time",
     },
     {
       title: "National Service",
       organization: "National Youth Service Corps",
       location: "Imo, Nigeria",
       date: "May 2015 - May 2016",
-      partFullTrainee: "Full-Time"
+      partFullTrainee: "Full-Time",
     },
   ];
 
@@ -56,15 +54,15 @@ const About = () => {
       organization: "TechQuest STEM Academy",
       location: "Lagos, Nigeria",
       date: "Aug 2023",
-      partFullTrainee: "Trainee"
+      partFullTrainee: "Trainee",
     },
     {
       title: "Diploma in Computer Application and Operations",
       organization: "Edutainment Computer School",
       location: "Lagos, Nigeria",
       date: "Sept 2009",
-      partFullTrainee: "Trainee"
-    }
+      partFullTrainee: "Trainee",
+    },
   ];
 
   const myEducation = [
@@ -73,107 +71,128 @@ const About = () => {
       organization: "Olabisi Onabanjo University",
       location: "Ogun, Nigeria",
       date: "Sept 2014",
-      partFullTrainee: "Full-Time"
-    }
+      partFullTrainee: "Full-Time",
+    },
   ];
 
   return (
     <>
       <div id={aboutCSS.about}>
-
         <div id={aboutCSS.abtMe}>
-
           <h1 id={aboutCSS.abtTitle}>About Me</h1>
           <h3 id={aboutCSS.abtText}>
-            A dedicated and innovative software engineer striving to connect, unify and make a great impact in the world of technology. A creative thinker, adept in software development and working with various data structures.
+            A dedicated and innovative software engineer striving to connect,
+            unify and make a great impact in the world of technology. A creative
+            thinker, adept in software development and working with various data
+            structures.
           </h3>
-
         </div>
 
         <div className={aboutCSS.innerBox}>
-
           <h1 className={aboutCSS.expTitle}>Experience</h1>
           <div>
-            {
-              myExperience.map((item, index) => {
-                return (
-                  <AboutMeProps key={index} title={item.title} organization={item.organization} location={item.location} date={item.date} partFullTrainee={item.partFullTrainee} />
-                )
-              })
-            }
+            {myExperience.map((item, index) => {
+              return (
+                <AboutMeProps
+                  key={index}
+                  title={item.title}
+                  organization={item.organization}
+                  location={item.location}
+                  date={item.date}
+                  partFullTrainee={item.partFullTrainee}
+                />
+              );
+            })}
           </div>
         </div>
 
         <div className={aboutCSS.innerBox}>
-
           <h1 className={aboutCSS.expTitle}>Training and Certification</h1>
           <div>
-            {
-              myTrainingAndCert.map((item, index) => {
-                return (
-                  <AboutMeProps key={index} title={item.title} organization={item.organization} location={item.location} date={item.date} partFullTrainee={item.partFullTrainee} />
-                )
-              })
-            }
+            {myTrainingAndCert.map((item, index) => {
+              return (
+                <AboutMeProps
+                  key={index}
+                  title={item.title}
+                  organization={item.organization}
+                  location={item.location}
+                  date={item.date}
+                  partFullTrainee={item.partFullTrainee}
+                />
+              );
+            })}
           </div>
         </div>
 
         <div className={aboutCSS.innerBox}>
-
           <h1 className={aboutCSS.expTitle}>Education</h1>
 
           <div>
-            {
-              myEducation.map((item, index) => {
-                return (
-                  <AboutMeProps key={index} title={item.title} organization={item.organization} location={item.location} date={item.date} partFullTrainee={item.partFullTrainee} />
-                )
-              })
-            }
+            {myEducation.map((item, index) => {
+              return (
+                <AboutMeProps
+                  key={index}
+                  title={item.title}
+                  organization={item.organization}
+                  location={item.location}
+                  date={item.date}
+                  partFullTrainee={item.partFullTrainee}
+                />
+              );
+            })}
           </div>
-
         </div>
-
       </div>
 
       <div id={aboutCSS.outro}>
-
-
         <div className={aboutCSS.leftOutro}>
-
           <ul>
-            <a href="http://linkedin.com/in/isaiah-okon" target="_blank" rel="noopener noreferrer">
+            <a
+              href="http://linkedin.com/in/isaiah-okon"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <i className="fa-brands fa-linkedin"></i>
             </a>
 
-
-            <a href="https://twitter.com/Izaya4real" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://twitter.com/Izaya4real"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <i className="fa-brands fa-twitter"></i>
             </a>
 
-            <a href="https://github.com/EazzyConnect" target="_blank" rel="noopener noreferrer">
+            <a
+              href="https://github.com/EazzyConnect"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <i className="fa-brands fa-github"></i>
             </a>
 
-            <li className={aboutCSS.bottomList}><a href="tel:+2348085202397"><i className="fa-solid fa-phone"></i> +2348085202397</a></li>
+            <li className={aboutCSS.bottomList}>
+              <a href="tel:+2348085202397">
+                <i className="fa-solid fa-phone"></i> +2348085202397
+              </a>
+            </li>
 
-            <li className={aboutCSS.bottomList}><a href="mailto:okon.isaiah.samuel@gmail.com"><i
-              className="fa-solid fa-envelope"></i> okon.isaiah.samuel@gmail.com</a></li>
+            <li className={aboutCSS.bottomList}>
+              <a href="mailto:okon.isaiah.samuel@gmail.com">
+                <i className="fa-solid fa-envelope"></i>{" "}
+                okon.isaiah.samuel@gmail.com
+              </a>
+            </li>
           </ul>
-
         </div>
 
         <div className={aboutCSS.rightOutro}>
-
           <h4 className={aboutCSS.rightOutroText}>©️ 2024</h4>
           <h4 className={aboutCSS.rightOutroText}>IsaiahOkon</h4>
-
         </div>
-
       </div>
-
     </>
-  )
+  );
 };
 
 export default About;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,54 +1,51 @@
-import React from 'react';
-import homeCSS from './css-files/Home.module.css';
-import Items from './sub-components/Items';
-import { Stack } from './sub-components/Items';
-import myPic from '/images/eazzy.png';
-
+import React from "react";
+import homeCSS from "./css-files/Home.module.css";
+import Items from "./sub-components/Items";
+import { Stack } from "./sub-components/Items";
+import myPic from "/images/eazzy.png";
 
 const Home = ({ toggleTab }) => {
-
-
   const myTechStack = [
     {
       image: "images/node.png",
-      alt: "Node.js"
+      alt: "Node.js",
     },
     {
       image: "images/react.png",
-      alt: "React.js"
+      alt: "React.js",
     },
     {
       image: "images/js.png",
-      alt: "JavaScript"
+      alt: "JavaScript",
     },
     {
       image: "images/html.png",
-      alt: "HTML"
+      alt: "HTML",
     },
     {
       image: "images/css.png",
-      alt: "CSS"
+      alt: "CSS",
     },
     {
       image: "images/typescript.png",
-      alt: "TypeScript"
+      alt: "TypeScript",
     },
     {
       image: "images/mongoDb.jpg",
-      alt: "MongoDB"
+      alt: "MongoDB",
     },
     {
       image: "images/postman.png",
-      alt: "PostMan"
+      alt: "PostMan",
     },
     {
       image: "images/github.png",
-      alt: "GitHub"
+      alt: "GitHub",
     },
     {
       image: "images/vsCode.jpg",
-      alt: "VsCode"
-    }
+      alt: "VsCode",
+    },
   ];
 
   const myProjects = [
@@ -56,62 +53,81 @@ const Home = ({ toggleTab }) => {
       projectImg: "images/project1.webp",
       imgAlt: "CV-Maker App",
       projectTitle: "CV-Creator Web Application",
-      projectDesc: "The application allows users to register, create, edit and download their CV for free.",
+      projectDesc:
+        "The application allows users to register, create, edit and download their CV for free.",
       projectStack: "Node.js, HBS, JavaScript, CSS, MongoDb",
       projectLink: "https://cv-maker-app.onrender.com/home",
       projectCode: "https://github.com/EazzyConnect/cvMaker_App",
       livePreview: "Live Preview",
       viewCode: "View Code",
       class1: "fa-solid fa-link",
-      class2: "fa-brands fa-github"
+      class2: "fa-brands fa-github",
     },
     {
       projectImg: "images/project5.jpg",
       imgAlt: "EMS App",
       projectTitle: "Employment Management System",
-      projectDesc: "The application allows employers to post jobs and job seekers to apply for jobs posted.",
+      projectDesc:
+        "The application allows employers to post jobs and job seekers to apply for jobs posted.",
       projectStack: "React.js, Node.js, Express.js, MongoDb",
       projectLink: "https://work-force-hub.onrender.com/",
       projectCode: "https://github.com/EazzyConnect/workForceHub_App",
       livePreview: "Live Preview",
       viewCode: "View Code",
       class1: "fa-solid fa-link",
-      class2: "fa-brands fa-github"
+      class2: "fa-brands fa-github",
     },
     {
       projectImg: "images/project4.webp",
       imgAlt: "TipCalculator",
       projectTitle: "Tip Calculator",
-      projectDesc: "The application is used to quickly determine the appropriate gratuity amount based on a percentage of the total bill.",
+      projectDesc:
+        "The application is used to quickly determine the appropriate gratuity amount based on a percentage of the total bill.",
       projectStack: "JavaScript, HTML, CSS",
       projectLink: "https://task-manager-zse1.onrender.com",
       projectCode: "https://github.com/EazzyConnect/taskManager",
       livePreview: "Live Preview",
       viewCode: "View Code",
       class1: "fa-solid fa-link",
-      class2: "fa-brands fa-github"
+      class2: "fa-brands fa-github",
     },
     {
       projectImg: "images/project9.jpg",
       imgAlt: "myTodoApp",
       projectTitle: "My Todo App",
-      projectDesc: "A sleek and intuitive to-do app designed to streamline your productivity and organize your tasks effortlessly",
+      projectDesc:
+        "A sleek and intuitive to-do app designed to streamline your productivity and organize your tasks effortlessly",
       projectStack: "Typescript, HTML, CSS",
       projectLink: "https://my-todo-application.onrender.com/",
-      projectCode: "https://github.com/EazzyConnect/SAIL_todoTask/tree/main/isaiahTodoTask",
+      projectCode:
+        "https://github.com/EazzyConnect/SAIL_todoTask/tree/main/isaiahTodoTask",
       livePreview: "Live Preview",
       viewCode: "View Code",
       class1: "fa-solid fa-link",
-      class2: "fa-brands fa-github"
-    }
+      class2: "fa-brands fa-github",
+    },
+    {
+      projectImg: "images/ai_img.png",
+      imgAlt: "chatbot",
+      projectTitle: "AI Chatbot",
+      projectDesc:
+        "A free chatbot designed to engage users in real-time interaction, dynamic and intelligent conversations",
+      projectStack: "React, Node.js, Express.js",
+      projectLink: "https://sail-aichatbot.onrender.com/",
+      projectCode: "",
+      livePreview: "Live Preview",
+      viewCode: "View Code",
+      class1: "fa-solid fa-link",
+      class2: "fa-brands fa-github",
+    },
   ];
 
   const downloadCV = () => {
     const CV = document.createElement("a");
     CV.href = "images/IsaiahOkon-CV.pdf";
     CV.download = "IsaiahOkon-CV.pdf";
-    CV.click()
-  }
+    CV.click();
+  };
 
   const connectWithMe = () => {
     toggleTab("Contact");
@@ -121,34 +137,38 @@ const Home = ({ toggleTab }) => {
   //   toggleTab("Projects");
   // };
 
-
   return (
     <>
       <div>
-
         <div id={homeCSS.heroSection}>
-
           <div id={homeCSS.heroLeft}>
-
             <h1 className={homeCSS.welcomeText}>
               Hi👋,
               <br />
               I'm <span>Isaiah Okon</span>,
-              <br />
-              A Software Engineer
+              <br />A Software Engineer
             </h1>
 
-            <h4 id={homeCSS.textBottom}> Ready to create extraordinary solutions with you! Let's embark on a journey of innovation and excellence together, building remarkable software that transforms ideas into reality. Together, we'll craft technology that leaves a lasting impact on the world. Let's build something amazing! 🚀
+            <h4 id={homeCSS.textBottom}>
+              {" "}
+              Ready to create extraordinary solutions with you! Let's embark on
+              a journey of innovation and excellence together, building
+              remarkable software that transforms ideas into reality. Together,
+              we'll craft technology that leaves a lasting impact on the world.
+              Let's build something amazing! 🚀
             </h4>
 
             <div id={homeCSS.heroBtnsWeb}>
-              <button id={homeCSS.downloadBtn} onClick={downloadCV}>Download CV</button>
-              <button id={homeCSS.ConnectBtn} onClick={connectWithMe} >Connect With Me</button>
+              <button id={homeCSS.downloadBtn} onClick={downloadCV}>
+                Download CV
+              </button>
+              <button id={homeCSS.ConnectBtn} onClick={connectWithMe}>
+                Connect With Me
+              </button>
             </div>
           </div>
 
           <div id={homeCSS.heroRight}>
-
             <img id={homeCSS.myPic} src={myPic} alt="isaiahOkon" />
 
             {/* <div id={homeCSS.heroLinks}>
@@ -170,147 +190,170 @@ const Home = ({ toggleTab }) => {
               </a>
 
             </div> */}
-
           </div>
 
           <div id={homeCSS.heroBtns}>
-            <button id={homeCSS.downloadBtn} onClick={downloadCV}>Download CV</button>
-            <button id={homeCSS.ConnectBtn} onClick={connectWithMe} >Connect With Me</button>
+            <button id={homeCSS.downloadBtn} onClick={downloadCV}>
+              Download CV
+            </button>
+            <button id={homeCSS.ConnectBtn} onClick={connectWithMe}>
+              Connect With Me
+            </button>
           </div>
-
         </div>
 
         <div id={homeCSS.services}>
-
           <div className={homeCSS.serviceHeading}>
             <h3 className={homeCSS.serviceTitle}>My Services</h3>
-            <p className={homeCSS.serviceDesc}>With expertise in both backend and frontend web development, I craft seamless digital experiences that marry functionality with aesthetic appeal. From robust server-side solutions to captivating user interfaces, I deliver dynamic websites that elevate your online presence.</p>
+            <p className={homeCSS.serviceDesc}>
+              With expertise in both backend and frontend web development, I
+              craft seamless digital experiences that marry functionality with
+              aesthetic appeal. From robust server-side solutions to captivating
+              user interfaces, I deliver dynamic websites that elevate your
+              online presence.
+            </p>
           </div>
 
           <div id={homeCSS.myServices}>
-
             <div id={homeCSS.serviceBox}>
               {/* <i className="fa-solid fa-database"></i> */}
-              <img className={homeCSS.servIcon} src="images/database.webp" alt="database" />
+              <img
+                className={homeCSS.servIcon}
+                src="images/database.webp"
+                alt="database"
+              />
               <h2>Backend Web Development</h2>
-              <p className={homeCSS.serviceBoxText}>Building the server-side logic and infrastructure that powers web applications, ensures security and
-                optimizng performance.</p>
+              <p className={homeCSS.serviceBoxText}>
+                Building the server-side logic and infrastructure that powers
+                web applications, ensures security and optimizng performance.
+              </p>
             </div>
 
             <div id={homeCSS.serviceBox}>
               {/* <i className="fa-solid fa-code"></i> */}
-              <img className={homeCSS.servIcon} src="images/webDev.jpg" alt="UI/UX" />
+              <img
+                className={homeCSS.servIcon}
+                src="images/webDev.jpg"
+                alt="UI/UX"
+              />
               <h2>Web Design</h2>
-              <p className={homeCSS.serviceBoxText}>Creating responsive websites that are visually appealing and user-friendly.</p>
+              <p className={homeCSS.serviceBoxText}>
+                Creating responsive websites that are visually appealing and
+                user-friendly.
+              </p>
             </div>
-
           </div>
-
         </div>
 
         <div id={homeCSS.tools}>
-
           <div className={homeCSS.toolsHeading}>
-
             <h3 className={homeCSS.toolsTitle}>My Tech Stack</h3>
             {/* <h6 className={homeCSS.toolsDescription}>Technologies I've recently worked with</h6> */}
-
           </div>
 
           <div className={homeCSS.techTools}>
-
-            {
-              myTechStack.map((stack, index) => {
-                return (
-                  <Stack key={index} stackImg={stack.image} stackAlt={stack.alt} />
-                )
-              })
-            }
-
+            {myTechStack.map((stack, index) => {
+              return (
+                <Stack
+                  key={index}
+                  stackImg={stack.image}
+                  stackAlt={stack.alt}
+                />
+              );
+            })}
           </div>
-
         </div>
 
         <div id={homeCSS.projects}>
-
           <div className={homeCSS.projectHeading}>
-
             <h3 className={homeCSS.projectTitle}>Projects</h3>
             {/* <h6 className={homeCSS.projectDescription}>Things I've built so far</h6> */}
-
           </div>
 
           <div className={homeCSS.projectSamples}>
-            {
-              myProjects.map((project, index) => {
-                return (
-                  <Items
-                    key={index}
-                    itemImg={project.projectImg}
-                    itemAlt={project.imgAlt}
-                    itemTitle={project.projectTitle}
-                    itemDesc={project.projectDesc}
-                    // itemStack={project.projectStack}
-                    itemLivePreview={project.projectLink}
-                    itemClass1={project.class1}
-                    itemPrevLink={project.livePreview}
-                    itemGithub={project.projectCode}
-                    itemClass2={project.class2}
-                    itemCode={project.viewCode} />
-                )
-              })
-            }
+            {myProjects.map((project, index) => {
+              return (
+                <Items
+                  key={index}
+                  itemImg={project.projectImg}
+                  itemAlt={project.imgAlt}
+                  itemTitle={project.projectTitle}
+                  itemDesc={project.projectDesc}
+                  // itemStack={project.projectStack}
+                  itemLivePreview={project.projectLink}
+                  itemClass1={project.class1}
+                  itemPrevLink={project.livePreview}
+                  itemGithub={project.projectCode}
+                  itemClass2={project.class2}
+                  itemCode={project.viewCode}
+                />
+              );
+            })}
           </div>
 
           {/* <div className={homeCSS.viewMore}>
             <button id={homeCSS.viewMoreBtn} onClick={viewMoreProject}>View More</button>
           </div> */}
-
         </div>
 
         <div id={homeCSS.outro}>
-
           <div className={homeCSS.leftOutro}>
-
             <ul>
-              <a className={homeCSS.linkIconHide} href="http://linkedin.com/in/isaiah-okon" target="_blank" rel="noopener noreferrer">
+              <a
+                className={homeCSS.linkIconHide}
+                href="http://linkedin.com/in/isaiah-okon"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 {/* <img className={homeCSS.linkIcon} src="icons/linkedIn.png" alt="linkedIn" /> */}
                 <i className="fa-brands fa-linkedin"></i>
               </a>
 
-              <a className={homeCSS.linkIconHide} href="https://twitter.com/Izaya4real" target="_blank" rel="noopener noreferrer">
+              <a
+                className={homeCSS.linkIconHide}
+                href="https://twitter.com/Izaya4real"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 {/* <img className={homeCSS.linkIcon} src="icons/twitter.png" alt="twitter" /> */}
                 <i className="fa-brands fa-twitter"></i>
               </a>
 
-              <a className={homeCSS.linkIconHide} href="https://github.com/EazzyConnect" target="_blank" rel="noopener noreferrer">
+              <a
+                className={homeCSS.linkIconHide}
+                href="https://github.com/EazzyConnect"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 {/* <img className={homeCSS.linkIcon} src="icons/github.png" alt="github" /> */}
                 <i className="fa-brands fa-github"></i>
               </a>
 
-              <li className={homeCSS.bottomList}><a href="tel:+2348085202397"><i className="fa-solid fa-phone"></i> +2348085202397</a></li>
+              <li className={homeCSS.bottomList}>
+                <a href="tel:+2348085202397">
+                  <i className="fa-solid fa-phone"></i> +2348085202397
+                </a>
+              </li>
 
-              <li className={homeCSS.bottomList}><a href="mailto:okon.isaiah.samuel@gmail.com"><i className="fa-solid fa-envelope"></i> okon.isaiah.samuel@gmail.com</a></li>
-
+              <li className={homeCSS.bottomList}>
+                <a href="mailto:okon.isaiah.samuel@gmail.com">
+                  <i className="fa-solid fa-envelope"></i>{" "}
+                  okon.isaiah.samuel@gmail.com
+                </a>
+              </li>
             </ul>
-
           </div>
 
           <div className={homeCSS.rightOutro}>
-
             <h4 className={homeCSS.rightOutroText}>©️ 2024</h4>
             <h4 className={homeCSS.rightOutroText}>IsaiahOkon</h4>
-
           </div>
-
         </div>
-
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Home;
-
 
 // itemClass1={"fa-solid fa-link"} itemClass2={"fa-brands fa-github"} itemPrevLink={"View Link"}
